ab84955186b59458cc38c1fee928ea68dab9e8fe
Bochkov Anton <bochkov.dev@yandex.ru>
2024-10-18 20:24:14 +0300

    Добавлена логика взаимодействия с БД.

---
+++ app/db/sessions.py
@@ -0,0 +1,19 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker
+
+from db.database import engine
+
+
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+
+def connection(method):
+    async def wrapper(*args, **kwargs):
+        async with async_session_maker() as session:
+            try:
+                return await method(*args, session=session, **kwargs)
+            except Exception as e:
+                await session.rollback()
+                raise e 
+            finally:
+                await session.close()
+    return wrapper
