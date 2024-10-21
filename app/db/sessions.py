1028359c7bd2f197f3d4c7dbf2c1ebff5f72f615
Бочков Антон <bochkov.dev@yandex.ru>
2024-10-21 21:36:00 +0300

    Изменен декоратор connection в db/session.py.

--- app/db/sessions.py
+++ app/db/sessions.py
@@ -6,11 +6,13 @@ from db.database import engine
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
-def connection(method):
+def connection(func):
     async def wrapper(*args, **kwargs):
         async with async_session_maker() as session:
             try:
-                return await method(*args, session=session, **kwargs)
+                result = await func(*args, session=session, **kwargs)
+                await session.commit()
+                return result
             except Exception as e:
                 await session.rollback()
                 raise e 
