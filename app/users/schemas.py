74fbed48b4a6ab81f8c339d8b2c7ba4692944e18
Бочков Антон <bochkov.dev@yandex.ru>
2024-10-21 21:39:30 +0300

    Добавлены схемы для модели users.models.User.

--- app/users/schemas.py
+++ app/users/schemas.py
@@ -1,3 +1,13 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, EmailStr
 
+
+class UserRegister(BaseModel):
+    username: str = Field(..., description='Никнейм')
+    email: EmailStr = Field(..., description='Email')
+    password: str = Field(..., min_length=8, max_length=50, description='Пароль')
+    password_check: str = Field(..., min_length=8, max_length=50, description='Пароль')
+
+class UserAuth(BaseModel):
+    email: EmailStr = Field(..., description='Email')
+    password: str = Field(..., min_length=8, max_length=50, description='Пароль')
 
