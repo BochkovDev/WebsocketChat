1252cd0a467de91a560d15070cb508990d32f08b
Бочков Антон <bochkov.dev@yandex.ru>
2024-10-21 21:41:29 +0300

    Добавлен CORSMiddleware, users.router include.

--- app/main.py
+++ app/main.py
@@ -1,10 +1,27 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from core.settings import settings
+from users.router import router as router_users
 
 
 app = FastAPI()
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'], 
+    allow_credentials=True,
+    allow_methods=['*'],  
+    allow_headers=['*'],  
+)
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+app.include_router(router_users)
