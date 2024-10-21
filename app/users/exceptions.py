12036e079c359d3fa0d575ee17c5d1af624d46d4
Бочков Антон <bochkov.dev@yandex.ru>
2024-10-21 21:37:01 +0300

    Добавлены Исключения users/exceptions.py.

---
+++ app/users/exceptions.py
@@ -0,0 +1,39 @@
+from typing import Any, Dict
+from fastapi import HTTPException, status
+
+
+class TokenExpiredException(HTTPException):
+    ''' Raises when the provided token has expired '''
+    def __init__(self, status_code: int = status.HTTP_401_UNAUTHORIZED, detail: Any = None, headers: Dict[str, str] | None = None) -> None:
+        detail = detail or 'Token has expired.'
+        headers = {'WWW-Authenticate': 'Bearer'} if headers is None else headers
+        super().__init__(status_code, detail, headers)
+
+
+class CredentialsException(HTTPException):
+    ''' Raises when can't validate user's credentials '''
+    def __init__(self, status_code: int = status.HTTP_401_UNAUTHORIZED, detail: Any = None, headers: Dict[str, str] | None = None) -> None:
+        detail = 'Could not validate credentials'
+        headers = {'WWW-Authenticate': 'Bearer'} if headers is None else headers
+        super().__init__(status_code, detail, headers)
+
+class UserAlreadyExistsException(HTTPException):
+    ''' Raises when trying to register an already existing user '''
+    def __init__(self, status_code: int = status.HTTP_409_CONFLICT, detail: Any = None, headers: Dict[str, str] | None = None) -> None:
+        detail = 'User already exists'
+        headers = {'WWW-Authenticate': 'Bearer'} if headers is None else headers
+        super().__init__(status_code, detail, headers)
+
+class InvalidCredentialsException(HTTPException):
+    ''' Raises when email or password is invalid '''
+    def __init__(self, status_code: int = status.HTTP_401_UNAUTHORIZED, detail: Any = None, headers: Dict[str, str] | None = None) -> None:
+        detail = detail or 'Invalid email or password'
+        headers = {'WWW-Authenticate': 'Bearer'} if headers is None else headers
+        super().__init__(status_code, detail, headers)
+
+class PasswordMismatchException(HTTPException):
+    ''' Raises when the provided password does not match the expected one '''
+    def __init__(self, status_code: int = status.HTTP_409_CONFLICT, detail: Any = None, headers: Dict[str, str] | None = None) -> None:
+        detail = detail or 'Password does not match.'
+        headers = {'WWW-Authenticate': 'Bearer'} if headers is None else headers
+        super().__init__(status_code, detail, headers)
